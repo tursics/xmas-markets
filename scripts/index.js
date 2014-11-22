7953f3d01dcb24b1a59ccf24103f215eb9ea64e7
Thomas Tursics <thomas@tursics.de>
2014-11-22 04:14:35 +0100

    Show only a condensed list for open, closed and faved markets

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 var config = this.config || {};
 config.timeout = 400;
-config.readyListAllMarkets = false;
 config.currentMarketId = 0;
 config.startDate = new Date();
 config.startDate.setFullYear( 2014, 10, 17); // month - 1
@@ -52,14 +51,14 @@ function getNextOpeningDays( obj)
 	while( workingDate < late) {
 		var daystr = dateToStr( workingDate);
     	if(( typeof obj[ daystr] !== 'undefined') && (obj[ daystr].trim() != '')) {
-    		break;
+			return days;
 	    }
 
 		++days;
 		workingDate.setDate( workingDate.getDate() + 1);
 	}
 
-	return days;
+	return 36500;
 }
 
 function getOpeningTime( obj, diffdays)
@@ -68,9 +67,9 @@ function getOpeningTime( obj, diffdays)
 	workingDate.setDate( workingDate.getDate() + diffdays);
 
 	var daystr = dateToStr( workingDate);
-   	if( typeof obj[ daystr] !== 'undefined') {
-   		return obj[ daystr].trim();
-    }
+	if( typeof obj[ daystr] !== 'undefined') {
+		return obj[ daystr].trim();
+	}
 
 	return '';
 }
@@ -79,6 +78,9 @@ function sortDataByDate( a, b)
 {
 	var daysA = getNextOpeningDays( a);
 	var daysB = getNextOpeningDays( b);
+
+	a.data_next_open = daysA;
+	b.data_next_open = daysB;
 
 	if( daysA == daysB) {
 		return getOpeningTime( a, daysA) > getOpeningTime( b, daysB);
@@ -104,10 +106,13 @@ function getNextMarketOpeningTime( obj)
 	var weekday = workingDate.getDay();
 	var diffNow = parseInt((workTime - nowTime) / 1000 / 60 / 60 / 24);
 	var diffDays = parseInt((workTime - mondayTime) / 1000 / 60 / 60 / 24);
+	var maxOpen = parseInt((config.endDate.getTime() - nowTime) / 1000 / 60 / 60 / 24);
 	var diffWeeks = parseInt(diffDays / 7);
 	var weekdays = new Array( "Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag");
 
-	if( 0 == days) {
+	if( maxOpen <= days) {
+		openingtime = 'Geschlossen';
+	} else if( 0 == days) {
 		openingtime = 'Heute von ' + getOpeningTime( obj, days) + ' Uhr';
 	} else if( 1 == days) {
 		openingtime = 'Morgen von ' + getOpeningTime( obj, days) + ' Uhr';
@@ -119,62 +124,69 @@ function getNextMarketOpeningTime( obj)
 		} else {
 			openingtime = 'Nächste Woche ' + weekdays[weekday];
 		}
-	} else if( true) {
-		openingtime = weekdays[weekday] + ' in ' + diffWeeks + ' Wochen';
 	} else {
-		openingtime = 'In ' + days + ' Tagen';
+		openingtime = weekdays[weekday] + ' in ' + diffWeeks + ' Wochen';
 	}
 
 	return openingtime;
 }
 
-function composeMarketItem( number)
+function composeMarketItem( obj)
 {
-    if( typeof data[ number] === 'undefined') {
-    	return '';
-    }
+	var openingtime = getNextMarketOpeningTime( obj);
 
-	var market = data[ number];
-	var openingtime = getNextMarketOpeningTime( market);
+	if( 'hide' == obj.todo) { return ''; }
+//	if( 'mail' == obj.todo) { return ''; }
+//	if( '' == obj.todo) { return ''; }
 
-	if( 'hide' == market.todo) { return ''; }
-//	if( 'mail' == market.todo) { return ''; }
-//	if( '' == market.todo) { return ''; }
+	var txt = '<p>' + obj.name + '</p><p>' + openingtime + '</p>';
+	var img = '<aside class="pack-begin"><img src="art/' + obj.id + '/128.jpg"></aside>';
 
-	var txt = '<p>' + market.name + '</p><p>' + openingtime + '</p>';
-	var img = '<aside class="pack-begin"><img src="art/' + market.id + '/128.jpg"></aside>';
-
-	return '<li>' + img + '<a href="javascript:callOneMarket(' + market.id + ');">' + txt + '</a></li>';
+	return '<li>' + img + '<a href="javascript:callOneMarket(' + obj.id + ');">' + txt + '</a></li>';
 }
 
 function fillListAllMarkets()
 {
-	if( config.readyListAllMarkets) {
-		return;
-	}
-	config.readyListAllMarkets = true;
-
 	var txt = '';
 
+	txt += '<header><i class="icon-heart-filled"></i> Meine Lieblingsmärkte</header>';
+	txt += '<li aria-disabled="true"><p>Kein Weihnachtsmarkt ausgewählt</p></li>';
+
  	data.sort( sortDataByDate);
+	var workingDate = new Date();
+	workingDate.setHours( 0, 0, 0, 0);
+ 	var nextOpen = -1;
+	var maxOpen = parseInt((config.endDate.getTime() - workingDate.getTime()) / 1000 / 60 / 60 / 24);
+
  	for( var i = 0; i < data.length; ++i) {
-		txt += composeMarketItem( i);
+ 		var obj = data[ i];
+
+ 		if( obj.data_next_open > nextOpen) {
+ 			if( maxOpen <= obj.data_next_open) {
+				txt += '<header><i class="icon-clock"></i> Geschlossene Märkte</header>';
+				nextOpen = 36500;
+ 			} else if( 0 == obj.data_next_open) {
+				txt += '<header><i class="icon-clock"></i> Heute geöffnet</header>';
+				nextOpen = obj.data_next_open;
+ 			} else if( 1 == obj.data_next_open) {
+				txt += '<header><i class="icon-clock"></i> Morgen geöffnet</header>';
+				nextOpen = obj.data_next_open;
+ 			} else {
+				txt += '<header><i class="icon-clock"></i> Weihnachtsmärkte</header>';
+				nextOpen = maxOpen;
+ 			}
+ 		}
+
+		txt += composeMarketItem( obj);
 	}
 
 	txt = composeList( txt);
 	txt = composeSectionList( txt);
-	document.querySelector('#allmarkets > article').innerHTML = txt;
-}
 
-document.querySelector('#btn-allmarkets').addEventListener('click', function() {
-	document.querySelector('#allmarkets').className = 'current';
-	document.querySelector('[data-position="current"]').className = 'left';
-	setTimeout( fillListAllMarkets, config.timeout);
-});
-document.querySelector('#btn-allmarkets-back').addEventListener('click', function() {
-	document.querySelector('#allmarkets').className = 'right';
-	document.querySelector('[data-position="current"]').className = 'current';
-});
+	txt = '<div style="margin:-1.5rem -1.5rem 1rem -1.5rem;"><img src="art/teaser.jpg" style="width:100%;"></div>' + txt;
+
+	document.querySelector('#drawer > article').innerHTML = txt;
+}
 
 // ·································································
 
@@ -242,7 +254,7 @@ function fillListOneMarket()
 
 	txt += '<div style="margin:0 -1.5rem 1rem -1.5rem;padding:1.5rem 1.5rem 0 1.5rem;text-align:center;border-bottom:1px solid #f97c17;background:#fde4d0;">';
 	txt += '<p style="color:#f97c17;">' + obj.name + '</p>';
-	txt += '<p><i class="icon-clock"></i>' + getNextMarketOpeningTime( obj) + ' geöffnet</p>';
+	txt += '<p><i class="icon-clock"></i> ' + getNextMarketOpeningTime( obj) + ' geöffnet</p>';
 	if( '' != obj.fee) {
 		txt += '<p>' + obj.fee + '</p>';
 	}
@@ -252,10 +264,10 @@ function fillListOneMarket()
 
 	txt += '<div style="margin:1rem -1.5rem 1rem -1.5rem;padding:0 1.5rem 0 1.5rem;text-align:center;border-top:1px solid #f97c17;border-bottom:1px solid #f97c17;background:#fde4d0;">';
 	txt += '<p><ul>';
-	txt += '<li style="float:left;padding:0 2rem 0 0;"><button><i class="icon-map"></i></button></li>';
+//	txt += '<li style="float:left;padding:0 2rem 0 0;"><button><i class="icon-map"></i></button></li>';
 	txt += '<li style="float:left;padding:0 2rem 0 0;"><button><i class="icon-star"></i></button></li>';
-	txt += '<li style="float:left;padding:0 2rem 0 0;"><button>Sharen</button></li>';
-	txt += '<li style="float:left;padding:0 2rem 0 0;"><button>BVG</button></li>';
+//	txt += '<li style="float:left;padding:0 2rem 0 0;"><button>Sharen</button></li>';
+//	txt += '<li style="float:left;padding:0 2rem 0 0;"><button>BVG</button></li>';
 	txt += '<li style="clear:both;"></li>';
 	txt += '</ul></p>';
 	txt += '<p>' + obj.street + ', ' + obj.zip_city + ' ' + obj.district + '</p>';
@@ -318,5 +330,9 @@ document.querySelector('#btn-onemarket-back').addEventListener('click', function
 	document.querySelector('#onemarket').className = 'right';
 	document.querySelector('[data-position="current"]').className = 'current';
 });
+
+// ·································································
+
+setTimeout( fillListAllMarkets, config.timeout);
 
 // ·································································
