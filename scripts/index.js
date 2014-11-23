3dd643c1d52e30a32aafc8045e3f7384a409d051
Thomas Tursics <thomas@tursics.de>
2014-11-24 00:50:12 +0100

    center calendar and made it smaller for smaller displays

--- scripts/index.js
+++ scripts/index.js
@@ -220,7 +220,7 @@ function composeMarketCalendar( obj)
 {
 	var txt = '';
 
-	txt += '<p><ul class="calendar">';
+	txt += '<p style="margin:0 -1.5rem 0 -1.5rem;"><ul class="calendar">';
 
 	var months = new Array( "Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez");
 	var workingDate = new Date();
@@ -278,21 +278,22 @@ function fillListOneMarket()
 	txt += '<p>' + obj.remarks + '</p>';
 
 	txt += '<div style="margin:1rem -1.5rem 1rem -1.5rem;padding:0 1.5rem 0 1.5rem;text-align:center;border-top:1px solid #f97c17;border-bottom:1px solid #f97c17;background:#fde4d0;">';
-	txt += '<p><ul>';
+	txt += '<p><ul style="margin:0 auto 0 auto;max-width:4rem;">';
+	txt += '<li style="float:left;padding:0 2rem 0 0;"><a href="javascript:callOneMarket(' + obj.id + ');" class="bb-button" style="font-size:3rem;min-width:4rem;min-height:4rem;text-align:center;padding:1rem 0 0 0;"><i class="icon-heart"></i></a></li>';
 //	txt += '<li style="float:left;padding:0 2rem 0 0;"><button><i class="icon-map"></i></button></li>';
-	txt += '<li style="float:left;padding:0 2rem 0 0;"><button><i class="icon-star"></i></button></li>';
 //	txt += '<li style="float:left;padding:0 2rem 0 0;"><button>Sharen</button></li>';
 //	txt += '<li style="float:left;padding:0 2rem 0 0;"><button>BVG</button></li>';
 	txt += '<li style="clear:both;"></li>';
 	txt += '</ul></p>';
+
 	txt += '<p>' + obj.street + ', ' + obj.zip_city + ' ' + obj.district + '</p>';
 	txt += '</div>';
 
 	txt += '<p>Öffnungszeiten</p>';
-	txt += composeMarketCalendar( obj);
 	txt += '<p>' + obj.hours + '</p>';
+	txt += composeMarketCalendar( obj);
 
-	txt += '<div style="margin:1rem -1.5rem 0 -1.5rem;padding:0 1.5rem 0 1.5rem;text-align:center;border-top:1px solid #f97c17;border-bottom:1px solid #f97c17;background:#fde4d0;">';
+	txt += '<div style="margin:1rem -1.5rem -1.5rem -1.5rem;padding:0 1.5rem 0 1.5rem;text-align:center;border-top:1px solid #f97c17;border-bottom:1px solid #f97c17;background:#fde4d0;">';
 	txt += '<p>Bildnachweis: ' + obj.copyright + '</p>';
 	txt += '</div>';
 
