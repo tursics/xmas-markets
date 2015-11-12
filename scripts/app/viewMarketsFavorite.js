31f61d7487447c8bc0d233b8ae5b5ed8446ead65
Thomas Tursics <thomas@tursics.de>
2015-11-12 17:32:01 +0100

    moved old (but never used) code to this file

---
+++ scripts/app/viewMarketsFavorite.js
@@ -0,0 +1,30 @@
+// ·································································
+
+define( ['app/config','app/view','app/sort'], function( config, view, sort) {
+	var idView = '#tab-market-favorite';
+
+	return {
+		init: function()
+		{
+			view.add( idView, 'Favoriten');
+
+			if( document.querySelector( idView) !== null) {
+				document.querySelector( idView).addEventListener( 'click', this.fillList.bind( this), false);
+			}
+		},
+		fillList: function()
+		{
+			view.setActive( idView);
+			view.showProgress();
+		 	sort.all();
+
+			var txt = '';
+
+			txt = '<div style="margin:-1.5rem -1.5rem 1rem -1.5rem;"><img src="art/teaser.jpg" style="width:100%;"></div>' + txt;
+
+			view.finishMarketList( txt);
+		}
+	};
+});
+
+// ·································································
