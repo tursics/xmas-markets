36561d01812970f57fb61fe340803aa09d652847
Thomas Tursics <thomas@tursics.de>
2015-11-12 17:34:56 +0100

    copied this code from the Moers version of this app

---
+++ scripts/app/viewMarketsNextDays.js
@@ -0,0 +1,74 @@
+// ·································································
+
+define( ['app/config','app/view','app/sort'], function( config, view, sort) {
+	var idView = '#tab-market-nextdays';
+
+	return {
+		init: function()
+		{
+			view.add( idView, 'Aktuell');
+
+			if( document.querySelector( idView) !== null) {
+				document.querySelector( idView).addEventListener( 'click', this.fillList.bind( this), false);
+			}
+		},
+		fillList: function()
+		{
+			view.setActive( idView);
+			view.showProgress();
+
+			var txt = '';
+			var sumtxt = '';
+
+		 	sort.today();
+			for( var i = 0; i < config.markets.length; ++i) {
+				var obj = config.markets[ i];
+				if( '' != obj.data_next_open) {
+					txt += view.composeMarketItem( obj, 0);
+				}
+			}
+			if( '' == txt) {
+				txt = '<li style="height:auto;"><p style="white-space:normal;line-height:2rem;">Heute gibt es keine Veranstaltungen</p></li>';
+			}
+			txt = '<header>Heute</header>' + txt;
+			sumtxt += txt;
+
+			txt = '';
+		 	sort.tomorrow();
+		 	for( var i = 0; i < config.markets.length; ++i) {
+ 				var obj = config.markets[ i];
+				if( '' != obj.data_next_open) {
+					txt += view.composeMarketItem( obj, 1);
+		 		}
+			}
+			if( '' == txt) {
+				txt = '<li style="height:auto;"><p style="white-space:normal;line-height:2rem;">Morgen gibt es keine Veranstaltungen</p></li>';
+			}
+			txt = '<header>Morgen</header>' + txt;
+			sumtxt += txt;
+
+			txt = '';
+		 	sort.overmorrow();
+		 	for( var i = 0; i < config.markets.length; ++i) {
+ 				var obj = config.markets[ i];
+				if( '' != obj.data_next_open) {
+					txt += view.composeMarketItem( obj, 2);
+ 				}
+			}
+			if( '' == txt) {
+				txt = '<li style="height:auto;"><p style="white-space:normal;line-height:2rem;">Übermorgen gibt es keine Veranstaltungen</p></li>';
+			}
+			txt = '<header>Übermorgen</header>' + txt;
+			sumtxt += txt;
+
+			txt = view.composeList( sumtxt);
+			txt = view.composeSectionList( txt);
+
+			txt = '<div style="margin:-1.5rem -1.5rem 1rem -1.5rem;"><img src="art/teaser.jpg" style="width:100%;"></div>' + txt;
+
+			view.finishMarketList( txt);
+		}
+	};
+});
+
+// ·································································
