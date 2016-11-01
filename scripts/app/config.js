6ac5b372f497e53d9be00bb454bf96af82b48d57
Thomas Tursics <thomas@tursics.de>
2016-11-01 17:20:47 +0100

    use also Brandenburg data

--- scripts/app/config.js
+++ scripts/app/config.js
@@ -1,41 +1,50 @@
-// ·································································
+/*jslint browser: true*/
+/*global define*/
 
-define( ['data/berlin', 'data/moers'], function( dataBerlin, dataMoers) {
-	var firstDay = 4000000000000;
-	var lastDay = 0;
-	var markets = [];
+//-----------------------------------------------------------------------
 
-	function addData( name, data)
-	{
-		markets = markets.concat( data);
+define(['data/berlin', 'data/brandenburg'], function (dataBerlin, dataBrandenburg) {
+	'use strict';
 
-		for( var market = 0; market < data.length; ++market) {
-			data[market]['path'] = name.toLowerCase();
-			firstDay = Math.min( firstDay, Date.parse( data[market]['begin']));
-			lastDay = Math.max( lastDay, Date.parse( data[market]['end']));
+	var firstDay = 4000000000000,
+		lastDay = 0,
+		market,
+		markets = [];
+
+	function addData(name, data) {
+		for (market = 0; market < data.length; ++market) {
+			data[market].path = name.toLowerCase();
+			if (!isNaN(Date.parse(data[market].begin))) {
+				firstDay = Math.min(firstDay, Date.parse(data[market].begin));
+			}
+			if (!isNaN(Date.parse(data[market].end))) {
+				lastDay = Math.max(lastDay, Date.parse(data[market].end));
+			}
 		}
+
+		markets = markets.concat(data);
 	}
 
-	addData( 'Berlin', dataBerlin.data);
-//	addData( 'Moers', dataMoers.data);
+	addData('Berlin', dataBerlin.data);
+	addData('Brandenburg', dataBrandenburg.data);
 
 	return {
 		timeout: 400,
 		currentMarketId: 0,
-		startDate: new Date( firstDay),
-		endDate: new Date( lastDay + (1000 * 60 * 60 * 24)),
-		views: new Array(),
+		startDate: new Date(firstDay),
+		endDate: new Date(lastDay + (1000 * 60 * 60 * 24)),
+		views: [],
 		markets: markets,
 		userLat: null,
 		userLon: null,
 
-		getMarketByID: function( marketId)
-		{
-			var path = marketId.split('-')[0];
-			var uuid = marketId.split('-')[1];
+		getMarketByID: function (marketId) {
+			var path = marketId.split('-')[0],
+				uuid = parseInt(marketId.split('-')[1], 10),
+				i;
 
-			for( var i = 0; i < this.markets.length; ++i) {
-				if(( this.markets[i].path == path) && ( this.markets[i].uuid == uuid)) {
+			for (i = 0; i < this.markets.length; ++i) {
+				if ((this.markets[i].path === path) && (this.markets[i].uuid === uuid)) {
 					return this.markets[i];
 				}
 			}
@@ -45,4 +54,4 @@ define( ['data/berlin', 'data/moers'], function( dataBerlin, dataMoers) {
 	};
 });
 
-// ·································································
+//-----------------------------------------------------------------------
