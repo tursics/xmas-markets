e21b040cbc436f510ca3106e5708a6f616656c64
Thomas Tursics <thomas@tursics.de>
2016-11-01 17:17:30 +0100

    hide map if no GPS data available

--- scripts/app/viewMarketsOne.js
+++ scripts/app/viewMarketsOne.js
@@ -1,52 +1,60 @@
-// ·································································
+/*jslint browser: true*/
+/*global require,define*/
 
-define( ['ajax','leaflet','app/config','app/view'], function( ajax, L, config, view) {
+//-----------------------------------------------------------------------
+
+define(['ajax', 'leaflet', 'app/config', 'app/view'], function (ajax, L, config, view) {
+	'use strict';
+
 	var idView = '#onemarket';
 
-//	function callChangeFavorite( marketId)
-//	{
+//	function callChangeFavorite (marketId) {
 //		document.querySelector('#buttonFav').innerHTML = '<i class="icon-heart-filled"></i>';
 //
 //		var obj = config.getMarketByID( marketId);
 //		utils.status.show( obj.name + ' ist jetzt dein Lieblingsevent.');
 //	}
 
-	function composeCalendar( obj)
-	{
-		var txt = '';
+	function composeCalendar(obj) {
+		var txt = '',
+			months = ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+			workingDate = new Date(),
+			weekday,
+			i,
+			style,
+			classStr,
+			daystr;
 
 		txt += '<p style="margin:0 -1.5rem 0 -1.5rem;"><ul class="calendar">';
 
-		var months = new Array( "Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez");
-		var workingDate = new Date();
-		workingDate.setFullYear( config.startDate.getFullYear(), config.startDate.getMonth(), config.startDate.getDate());
+		workingDate.setFullYear(config.startDate.getFullYear(), config.startDate.getMonth(), config.startDate.getDate());
 
-		var weekday = (workingDate.getDay() + 6) % 7;
-		for( var i = 0; i < weekday; ++i) {
+		weekday = (workingDate.getDay() + 6) % 7;
+		for (i = 0; i < weekday; ++i) {
 			txt += '<li class="empty"></li>';
 		}
 
-		while( workingDate <= config.endDate) {
-			var style = '';
-			if( 1 == workingDate.getDay()) {
+		while (workingDate <= config.endDate) {
+			style = '';
+			if (1 === workingDate.getDay()) {
 				style = ' style="clear:both;"';
 			}
 
-			var classStr = '';
-			var daystr = view.dateToStr( workingDate);
-	   		if(( typeof obj[ daystr] === 'undefined') || ('' == obj[ daystr].trim())) {
-	   			classStr = ' class="gray"';
-    		}
+			classStr = '';
+			daystr = view.dateToStr(workingDate);
+			if ((typeof obj[daystr] === 'undefined') || ('' === obj[daystr].trim())) {
+				classStr = ' class="gray"';
+			}
 
 			txt += '<li' + classStr + style + '>' + workingDate.getDate() + '<div>' + months[workingDate.getMonth()] + '</div></li>';
 
-			workingDate.setDate( workingDate.getDate() + 1);
+			workingDate.setDate(workingDate.getDate() + 1);
 		}
 
-		while( workingDate.getDay() != 1) {
+		while (workingDate.getDay() !== 1) {
 			txt += '<li class="gray">' + workingDate.getDate() + '<div>' + months[workingDate.getMonth()] + '</div></li>';
 
-			workingDate.setDate( workingDate.getDate() + 1);
+			workingDate.setDate(workingDate.getDate() + 1);
 		}
 
 		txt += '</ul></p>';
@@ -56,29 +64,33 @@ define( ['ajax','leaflet','app/config','app/view'], function( ajax, L, config, v
 	}
 
 	return {
-		init: function()
-		{
+		init: function () {
 		},
-		fillList: function()
-		{
-			if( typeof view === 'undefined') {
-				view = require( 'app/view');
+		fillList: function () {
+			if (typeof view === 'undefined') {
+				view = require('app/view');
 			}
 
-			var txt = '';
-			var obj = config.getMarketByID( config.currentMarketId);
+			var txt = '',
+				obj = config.getMarketByID(config.currentMarketId),
+				opening = view.getNextMarketOpeningTime(obj),
+				date,
+				mapboxToken,
+				mapboxTiles,
+				map,
+				mapPin;
+
+//			if (opening !== 'Vorbei') {
+//				opening += ' geöffnet';
+//			}
 
 			txt += '<div style="margin:-1.5rem -1.5rem -1rem -1.5rem;"><img src="art/' + obj.path + '/' + obj.uuid + '/1200.jpg" style="width:100%;"></div>';
 
 			txt += '<div style="margin:0 -1.5rem 1rem -1.5rem;padding:1.5rem 1.5rem 0 1.5rem;text-align:center;border-bottom:1px solid #f97c17;background:#fde4d0;">';
 			txt += '<p style="color:#f97c17;">' + obj.name + '</p>';
 
-			var opening = view.getNextMarketOpeningTime( obj);
-			if( opening != 'Vorbei') {
-//				opening += ' geöffnet';
-			}
 			txt += '<p><i class="icon-clock"></i> ' + opening + '</p>';
-			if( '' != obj.fee) {
+			if ('' !== obj.fee) {
 				txt += '<p>' + obj.fee + '</p>';
 			}
 			txt += '</div>';
@@ -96,20 +108,20 @@ define( ['ajax','leaflet','app/config','app/view'], function( ajax, L, config, v
 //			txt += '</ul></p>';
 			txt += '<div id="map" style="height:25rem;margin:0 -1.5rem 0 -1.5rem;border-bottom:1px solid #f97c17;"></div>';
 
-			if( typeof obj.zip_city !== 'undefined') {
+			if (typeof obj.zip_city !== 'undefined') {
 				txt += '<p>' + obj.street + ', ' + obj.zip_city + ' ' + obj.district + '</p>';
 			} else {
 				txt += '<p>' + obj.street + ', ' + obj.zip + ' ' + obj.city + '</p>';
 			}
 			txt += '</div>';
 
-			if( obj.begin == obj.end) {
-				var date = new Date( obj.begin);
+			if (obj.begin === obj.end) {
+				date = new Date(obj.begin);
 				txt += '<p>Datum: ' + date.getDate() + '.' + (date.getMonth() + 1) + "." + date.getFullYear() + '<br>Uhrzeit: ' + obj.hours + ' Uhr</p>';
 			} else {
 				txt += '<p>Öffnungszeiten</p>';
 				txt += '<p>' + obj.hours + '</p>';
-				txt += composeCalendar( obj);
+				txt += composeCalendar(obj);
 			}
 
 			txt += '<div style="margin:1rem -1.5rem -1.5rem -1.5rem;padding:0 1.5rem 0 1.5rem;text-align:center;border-top:1px solid #f97c17;border-bottom:1px solid #f97c17;background:#fde4d0;">';
@@ -117,29 +129,34 @@ define( ['ajax','leaflet','app/config','app/view'], function( ajax, L, config, v
 			txt += '</div>';
 
 			document.querySelector('#onemarket > article').innerHTML = txt;
-			document.querySelector('#onemarket > article').scrollTo( 0, 0);
+			document.querySelector('#onemarket > article').scrollTo(0, 0);
 
-			ajax.get( 'art/' + obj.path + '/' + obj.uuid + '/LICENSE.md', {}, function( text) {
-				document.querySelector('#copyright').innerHTML = 'Bildnachweis: ' + text;
-			});
+			try {
+				ajax.get('art/' + obj.path + '/' + obj.uuid + '/LICENSE.md', {}, function (text) {
+					document.querySelector('#copyright').innerHTML = 'Bildnachweis: ' + text;
+				}, true);
+			} catch (e) {
+			}
 
-			var mapboxToken = 'pk.eyJ1IjoidHVyc2ljcyIsImEiOiJjaWh3Z3ZlNGYwMm01dWtrbzEyc3o5Z2l2In0.y9Lzc24BygGS_lmbpfRpxg';
-			var mapboxTiles = L.tileLayer( 'https://{s}.tiles.mapbox.com/v4/tursics.l7ad5ee8/{z}/{x}/{y}.png?access_token=' + mapboxToken, {
-				attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
-			});
+			if ((obj.lat !== null) && (obj.lng !== null)) {
+				mapboxToken = 'pk.eyJ1IjoidHVyc2ljcyIsImEiOiJjaWh3Z3ZlNGYwMm01dWtrbzEyc3o5Z2l2In0.y9Lzc24BygGS_lmbpfRpxg';
+				mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v4/tursics.l7ad5ee8/{z}/{x}/{y}.png?access_token=' + mapboxToken, {
+					attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+				});
 
-			var map = L.map('map', {zoomControl: false})
-				.addLayer( mapboxTiles)
-				.setView([obj.lat, obj.lng], 16);
+				map = L.map('map', {zoomControl: true, scrollWheelZoom: false})
+					.addLayer(mapboxTiles)
+					.setView([obj.lat, obj.lng], 16);
 
-			var circle = L.circle([obj.lat, obj.lng], 25, {
-				color:'#000',
-				fillColor:'#F97C17',
-				fillOpacity:0.5,
-				weight:1
-			}).addTo(map);
+				mapPin = L.circle([obj.lat, obj.lng], 25, {
+					color: '#000',
+					fillColor: '#F97C17',
+					fillOpacity: 0.5,
+					weight: 1
+				}).addTo(map);
+			}
 		}
 	};
 });
 
-// ·································································
+//-----------------------------------------------------------------------
