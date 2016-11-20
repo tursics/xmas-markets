8955ff85c21aca613eb18942c55a7a84f9e3db66
Thomas Tursics <thomas@tursics.de>
2016-11-20 21:52:11 +0100

    uuid must be a integer

--- build/build_berlin.js
+++ build/build_berlin.js
@@ -412,7 +412,7 @@ function getRefData(obj) {
 //			return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 //		}
 //		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-		return Math.floor((1 + Math.random()) * 0x10000).toString(10);
+		return parseInt(Math.floor((1 + Math.random()) * 0x10000).toString(10), 10);
 	}
 
 	var i = 0,
